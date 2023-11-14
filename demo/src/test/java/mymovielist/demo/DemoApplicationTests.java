d126a3e62a3c9f05924be560ad66e5d07549f98d
mouraGustavo <moura.gborges@gmail.com>
2023-11-13 21:52:45 -0300

    first push

---
+++ demo/src/test/java/mymovielist/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mymovielist.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
