84f1c9a7ce8b36ee8046ac0bd5952d0a07cbe0f2
mouraGustavo <moura.gborges@gmail.com>
2023-11-23 12:08:49 -0300

    Criando crud usuário

--- movielist/src/main/java/com/pw/movielist/principal/model/Usuario.java
+++ movielist/src/main/java/com/pw/movielist/principal/model/Usuario.java
@@ -33,8 +33,32 @@ public class Usuario {
         this.id = id;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public List<Lista> getListas() {
         return listas;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
     public void setListas(List<Lista> listas) {
---
+++ movielist/src/main/java/com/pw/movielist/principal/model/dto/UsuarioDTO.java
@@ -0,0 +1,52 @@
+package com.pw.movielist.principal.model.dto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.pw.movielist.principal.model.Usuario;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UsuarioDTO {
+    private long id;
+    private String nome;
+    private String email;
+    private String senha;
+    
+    public UsuarioDTO(Usuario usuario) {
+        this.id = usuario.getId();
+        this.nome = usuario.getNome();
+        this.email = usuario.getEmail();
+        this.senha = usuario.getSenha();
+    }
+    public UsuarioDTO(long id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public UsuarioDTO(){
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
