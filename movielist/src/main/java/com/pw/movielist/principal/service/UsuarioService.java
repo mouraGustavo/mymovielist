8a54f336b4b208c7087e5a80a0283f76ce1d63ef
Italo Vilas Boas <vpn-990882@adm.bancoamazonia.sa>
2023-11-25 18:44:03 -0300

    Atualizar cadastro do usuario

--- movielist/src/main/java/com/pw/movielist/principal/service/UsuarioService.java
+++ movielist/src/main/java/com/pw/movielist/principal/service/UsuarioService.java
@@ -1,10 +1,12 @@
 package com.pw.movielist.principal.service;
 
+import com.pw.movielist.principal.exception.NotFoundException;
 import com.pw.movielist.principal.model.Usuario;
 import com.pw.movielist.principal.model.dto.UsuarioDTO;
 import com.pw.movielist.principal.repository.UsuarioRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,16 @@ public class UsuarioService{
 
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
+    }
+
+    public ResponseEntity<UsuarioDTO> atualizarUsuario(Long idUsuario, UsuarioDTO usuarioDTO){
+        Optional<Usuario> usuarioAtualizar = usuarioRepository.findById(idUsuario);
+        if(usuarioAtualizar.isPresent()){
+            usuarioAtualizar.get().setNome(usuarioDTO.getNome());
+            usuarioAtualizar.get().setSenha(usuarioDTO.getSenha());
+            return ResponseEntity.status(HttpStatus.OK).body(new UsuarioDTO(usuarioRepository.save(usuarioAtualizar.get())));
+        }
+        throw new NotFoundException("usuario: " + idUsuario + " não encontrado");
     }
 
     public ResponseEntity<UsuarioDTO> criarUsuario(UsuarioDTO usuarioRequest){
